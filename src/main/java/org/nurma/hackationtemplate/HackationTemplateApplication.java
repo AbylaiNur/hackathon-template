dbb151a0171264c3c0fcc023085b8eaf66b3bc2c
AbylaiNur <abylai.nurskenev@gmail.com>
2024-03-31 16:02:15 +0500

    first commit

---
+++ src/main/java/org/nurma/hackationtemplate/HackationTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.nurma.hackationtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackationTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackationTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nurma/hackationtemplate/HackationTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nurma.hackationtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackationTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
